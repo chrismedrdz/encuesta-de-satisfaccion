6efa5aaf77369e4aeabb0e4108789eec66baa44a
Alvaro <alvaro_aer@hotmail.com>
2017-03-28 15:34:39 -0600

    Time to finish

--- resources/views/client/finish.blade.php
+++ resources/views/client/finish.blade.php
@@ -1,9 +1,22 @@
 <div class="col_full topmargin center">	
 	<img class="img-gracias" src="{{ url('/images/gracias.png') }} " style="margin-bottom:40px;">
-	<H1 style="color: black;">Haz terminado satisfactoriamente la Encuesta.</H1>
+	<H1 style="color: black;">Haz terminado satisfactoriamente la Encuesta.</H1></br>
+	<h2 style="color: black;"> En un momento serás redireccionado </h2>
 </div>
 
 <div class="col_full nobottommargin center">
 
-	<a class="button button-3d button-red nomargin" id="login-form-logout" name="login-form-logout" type="button"  href="{{ url('/logout', [$usuario_id . 'e']) }}">Salir</a>
+	<a class="button button-3d button-red nomargin" id="login-form-logout terminar" name="login-form-logout" type="button"  href="{{ url('/logout', [$usuario_id . 'e']) }}">Salir</a>
 </div>
+
+<script>
+@php 
+$url =action('Auth\LoginController@logout', [$usuario_id . 'e']);
+@endphp
+	function terminado(){
+		 window.location.href ="{{$url}}";
+	}
+
+	setInterval(terminado, 6350);
+
+</script>
